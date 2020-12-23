6b67d16d9c84d9f5eefaffbe11085d88158dc292
DhakalRupesh <a73dhakal@outlook.com>
2020-12-23 16:08:38 +0545

    card complete

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -10,11 +10,12 @@ import { MdLocationOn } from "react-icons/md";
 import { FaBed } from "react-icons/fa";
 import { GiKnifeFork } from "react-icons/gi";
 import { FaBath } from "react-icons/fa";
+import { FaPhoneAlt } from "react-icons/fa";
 
 import "./Card.css";
 import { NavLink } from "react-router-dom";
 
-export default function index() {
+export default function Card(props) {
   return (
     <IconContext.Provider value={{ color: "#28c092", size: "20px" }}>
       <>
@@ -30,12 +31,17 @@ export default function index() {
                     <div className="col-md-4 col-10 mx-auto">
                       <div className="card shadow about-card">
                         <div className="card-body">
+                          {/* title */}
                           <img
                             alt="First slide"
                             className="d-block w-100"
                             src={slider1}
                           />
-                          <div className="tag shadow" name="ppfor">
+                          <div
+                            className="tag shadow"
+                            name="ppfor"
+                            style={props.css}
+                          >
                             For Sale
                           </div>
                           <h4 className=" text-center text-capitalize font-weight-bold pt-3">
@@ -47,9 +53,37 @@ export default function index() {
                             </span>
                             Sinamangal, Kathmandu
                           </p>
+                          {/* feature */}
+                          <div className="facilities row pb-3 pt-4">
+                            <div className="col-md-5 mx-auto">
+                              <span className="pr-2">
+                                <FaBed />
+                              </span>
+                              3 Bedrooms
+                            </div>
+                            <div className="col-md-5 mx-auto">
+                              <span className="pr-2">
+                                <GiKnifeFork />
+                              </span>
+                              2 Kitchen
+                            </div>
+                          </div>
 
-                          <div className="facilities">hello how are you ?</div>
-
+                          <div className="facilities1 row pb-4">
+                            <div className="col-md-5 mx-auto">
+                              <span className="pr-2">
+                                <FaBath />
+                              </span>
+                              3 Bathroom
+                            </div>
+                            <div className="col-md-5 mx-auto">
+                              <span className="pr-2">
+                                <FaPhoneAlt />
+                              </span>
+                              9865231457
+                            </div>
+                          </div>
+                          {/* feature end */}
                           <NavLink
                             type="button"
                             className="btn btn-lg btn-block shadow mt-3 viewdetails"
